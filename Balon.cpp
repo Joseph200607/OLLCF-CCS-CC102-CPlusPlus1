ebc7fbf660f1e12a2b8aa5008b6c3866dfb43c12
Joseph200607 <ac6803980@gmail.com>
2024-10-15 10:38:55 +0800

    Add files via upload

---
+++ Balon.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int sugarPriceUSD=10.23, ricePricePound=1.23, sardinesPricePound=0.5, coffePriceUSD=3.23, milkPriceUSD=4.12;
+	float USDtoPHP, PoundstoPHP, totalPHP,
+	float sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+	double usdollars;
+	double Pounds;
+	double PHP;
+	
+	cout << "Enter the quantity of sugar: " << endl;
+	cin >> sugarQty;
+	cout << "Enter the quantity of rice: " << endl;
+	cin >> riceQty;
+	cout << "Enter the quantity of sardines: " << endl;
+	cin >> sardinesQty;
+	cout << "Enter the quantity of coffee: " << endl;
+	cin >> coffeeQty;
+	cout << "Enter the qunatity of milk: " << endl;
+	cin >> milkQty;
+	
+	
+	usd conversion//
+	USDtpPHP, PoundtoPHP, TotalPHP,
+	
+	cout << "Price of sugar in PHP";
+	cout << "Price of rice in PHP";
+	cout << "Price of sardines in PHP";
+	cout << "Price of coffee in PHP";
+	cout << "Price of milk in PHP";
+	cout << "Total cost";
+	return 0;
+}
+
+	
+	
+	
+
